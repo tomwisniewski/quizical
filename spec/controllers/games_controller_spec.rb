1700e547ec68b62ddeae8db2a691b96ef3fdc239
Tom Wisniewski <tom.a.wisniewski@gmail.com>
2013-07-10 16:47:28 +0100

    spec test outlines created

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -9,23 +9,9 @@ describe GamesController do
     end
   end
 
-  describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
-    end
-  end
-
   describe "GET 'show'" do
     it "returns http success" do
-      get 'show'
-      response.should be_success
-    end
-  end
-
-  describe "GET 'update'" do
-    it "returns http success" do
-      get 'update'
+      get 'show', :id => 1
       response.should be_success
     end
   end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,32 @@ require 'spec_helper'
 
 describe UsersController do
 
+  describe "GET 'show'" do
+    it "returns http success" do
+      get 'show', :id => 1
+      response.should be_success
+    end
+  end
+
+  describe "GET 'index'" do
+    it "returns http success" do
+      get 'index'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'new'" do
+    it "returns http success" do
+      get 'new'
+      response.should be_success
+    end
+  end
+
+  describe "GET 'edit'" do
+    it "returns http success" do
+      get 'edit', id: 1
+      response.should be_success
+    end
+  end
+
 end
