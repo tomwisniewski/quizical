92654b7c3a2656ccad087eda05e5d15750374ba7
tboeckmann <trb@bespecific.co.uk>
2013-07-10 18:18:28 +0100

    Add questions show page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,9 +1,11 @@
 class QuestionsController < ApplicationController
 
   def index
+    @questions = Question.find_by_user_id(session[:user_id])
   end
 
   def new
+
   end
 
   def create
@@ -12,10 +14,16 @@ class QuestionsController < ApplicationController
     q.text = params[:question][:text]
     q.answer = params[:question][:answer]
     q.category = params[:question][:category]
-
+    if q.valid?
+      q.save
+      redirect_to "questions#index"
+    else
+      redirect_to "#", notice: q.valid?.errors.full_messages
+    end
   end
 
   def show
+    @questions = Question.find_by_user_id(session[:user_id])
   end
 
   def update
